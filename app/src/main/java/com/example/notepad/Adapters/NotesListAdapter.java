a43891feb19b7384a8884acd43247a5b1f11b848
SVitaliiR <vitaliyruslanov@gmail.com>
2023-11-21 01:42:24 +0200

    Added ui

---
+++ app/src/main/java/com/example/notepad/Adapters/NotesListAdapter.java
@@ -0,0 +1,18 @@
+package com.example.notepad.Adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NotesListAdapter {
+
+}
+
+class NotesViewHolder extends RecyclerView.ViewHolder {
+
+
+    public NotesViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
