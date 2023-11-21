728b857e1f2c1b9c8ef2caa058645211c6f873f2
SVitaliiR <vitaliyruslanov@gmail.com>
2023-11-21 05:36:41 +0200

    Finale commit

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.notepad;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.example.notepad.Adapters.NotesListAdapter;
 import com.example.notepad.Database.RoomDB;
@@ -18,12 +26,15 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
     RecyclerView recyclerView;
     NotesListAdapter notesListAdapter;
     List<Notes> notes = new ArrayList<>();
     RoomDB database;
     FloatingActionButton fab_add;
+    SearchView searchView_home;
+    Notes selectedNote;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +44,10 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycler_home);
         fab_add = findViewById(R.id.fab_add);
 
+        searchView_home = findViewById(R.id.searchView_home);
         database = RoomDB.getInstance(this);
         notes = database.dataAccessObject().getAll();
+
 
         updateRecycler(notes);
 
@@ -45,6 +58,53 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 101);
             }
         });
+
+        searchView_home.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filter(newText);
+                return true;
+            }
+        });
+    }
+
+    private void filter(String newText) {
+        List<Notes> filteredList = new ArrayList<>();
+        for (Notes singleNote : notes){
+            if (singleNote.getTitle().toLowerCase().contains(newText.toLowerCase())
+            || singleNote.getNotes().toLowerCase().contains(newText.toLowerCase())){
+                filteredList.add(singleNote);
+            }
+        }
+        notesListAdapter.filterList(filteredList);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 101){
+            if (resultCode == Activity.RESULT_OK){
+                Notes new_notes = (Notes) data.getSerializableExtra("note");
+                database.dataAccessObject().insert(new_notes);
+                notes.clear();
+                notes.addAll(database.dataAccessObject().getAll());
+                notesListAdapter.notifyDataSetChanged();
+            }
+        } else if (requestCode == 102) {
+            if (resultCode == Activity.RESULT_OK){
+                Notes new_notes = (Notes) data.getSerializableExtra("note");
+                database.dataAccessObject().toUpdate(new_notes.getID(), new_notes.getTitle(), new_notes.getNotes());
+                notes.clear();
+                notes.addAll(database.dataAccessObject().getAll());
+                notesListAdapter.notifyDataSetChanged();
+            }
+        }
     }
 
     private void updateRecycler(List<Notes> notes) {
@@ -57,12 +117,37 @@ public class MainActivity extends AppCompatActivity {
     private final NotesClickListener notesClickListener = new NotesClickListener() {
         @Override
         public void onClick(Notes notes) {
-
+            Intent intent = new Intent(MainActivity.this, NotesTakerActivity.class);
+            intent.putExtra("old_note", notes);
+            startActivityForResult(intent, 102);
         }
 
         @Override
         public void onLongClick(Notes notes, CardView cardView) {
-
+            selectedNote = new Notes();
+            selectedNote = notes;
+            showPopup(cardView);
         }
     };
+
+    private void showPopup(CardView cardView) {
+        PopupMenu popupMenu = new PopupMenu(this, cardView);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.popup_menu);
+        popupMenu.show();
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if (item.getItemId() == R.id.delete) {
+            database.dataAccessObject().delete(selectedNote);
+            notes.remove(selectedNote);
+            notesListAdapter.notifyDataSetChanged();
+            Toast.makeText(MainActivity.this, "Note Deleted", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+    }
+
 }
